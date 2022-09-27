5e19064508473daf66321f007794189fae19d2b8
Acer <Acer@DESKTOP-K09QF87>
2022-09-27 18:57:54 +0200

    Added comments data in newsletter.php

---
+++ newsletter.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: Newsletter
+ * Description: This plugin allows send email to subscribers
+ * Version: 1.0
+ * Requires at least: 5.0
+ * Requires PHP: 7.4
+ * Author: Stefano Puggioni
+ * License: GPL v2 or later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ */
+?>
