4f3384fef2a97721391b7cddd9689a487161a43f
Acer <Acer@DESKTOP-K09QF87>
2022-09-27 19:46:05 +0200

    Added setSqlCreate method in table users.php

--- classes/database/tables/table.php
+++ classes/database/tables/table.php
@@ -4,16 +4,26 @@ namespace Newsletter\Classes\Database\Tables;
 
 use Newsletter\Exceptions\NotSettedException;
 use Newsletter\Classes\Database\Tables\TableErrors as Te;
+use wpdb;
 
 abstract class Table implements Te{
-    private string $name;
+    protected wpdb $wpdb;
+    protected string $name;
+    protected string $charset;
+    protected string $sql_create;
+    protected string $sql_drop;
 
     public function __construct(array $data){
+        global $wpdb;
+        $this->wpdb = $wpdb;
         $ok = $this->assignValues($data);
         if(!$ok)throw new NotSettedException(Te::NOTISSET_EXC);
     }
 
     public function getName(){return $this->name;}
+    public function getCharset(){return $this->charset;}
+    public function getSqlCreate(){return $this->sql_create;}
+    public function getSqlDrop(){return $this->sql_drop;}
 
     /**
      * Assign the entry data to the proper properties
@@ -22,6 +32,7 @@ abstract class Table implements Te{
         $isset = false;
         if(isset($data['name'])){
             $this->name = $data['name'];
+            $this->charset = $this->wpdb->get_charset_collate();
         }//if(isset($data['name'])){
         return $isset;
     }
--- classes/database/tables/users.php
+++ classes/database/tables/users.php
@@ -4,16 +4,32 @@ namespace Newsletter\Classes\Database\Tables;
 
 class Users extends Table{
 
-    private string $sql_create;
-    private string $sql_drop;
-
     public function __construct(array $data)
     {
         parent::__construct($data);
     }
 
-    public function getSqlCreate(){return $this->sql_create;}
-    public function getSqlDrop(){return $this->sql_drop;}
+    /**
+     * Set the creation query for this table
+     */
+    private function setSqlCreate(): void{
+        $this->sql_create = <<<SQL
+        CREATE TABLE `{$this->name}` (
+          `id` int(11) NOT NULL AUTO_INCREMENT,
+          `email` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+          `lang` varchar(3) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'User language code',
+          `codVer` varchar(191) CHARACTER SET utf8mb4 DEFAULT NULL COMMENT 'Verification code ',
+          `codDis` varchar(191) CHARACTER SET utf8mb4 DEFAULT NULL COMMENT 'Unsubscribe code',
+          `subscribed` tinyint(1) DEFAULT NULL COMMENT '0 = email not verified\r\n1 = email verified',
+          `subscDate` datetime DEFAULT NULL COMMENT 'Date which user requested the subscribe to the newsletter',
+          `ActDate` datetime DEFAULT NULL COMMENT 'Date which user activated his account',
+          PRIMARY KEY (`id`),
+          UNIQUE KEY `email` (`email`),
+          UNIQUE KEY `codVer` (`codVer`),
+          UNIQUE KEY `codDis` (`codDis`)
+        ) ENGINE=InnoDB {$this->charset};
+        SQL;
+    }
 }
 
 ?>
