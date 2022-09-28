94da4fcc0b6a5da5eb37d5192f5a70c21fb52350
Acer <Acer@DESKTOP-K09QF87>
2022-09-28 19:26:48 +0200

    Added privacy,cookie,terms document links in subscribeFormValues method of properties.php class

--- classes/properties.php
+++ classes/properties.php
@@ -51,27 +51,30 @@ class Properties{
      */
     public static function subscribeFormValues(Langs $lang):array {
         $values = [];
+        $privacyUrl = Properties::privacyUrl($lang);
+        $cookieUrl = Properties::cookieUrl($lang);
+        $termsUrl = Properties::termsUrl($lang);
         if($lang == Langs::Italian){
             $values = [
                 "title" => "Newsletter","ea_title" => "Indirizzo email",
-                "cb_label1" => "Dichiaro di aver letto l' informativa sulla privacy e sui cookie, acconsento al trattamento dei miei dati personali",
-                "cb_label2" => "Ho letto e accettato i Termini e condizioni del servizio",
+                "cb_label1" => "Dichiaro di aver letto l' <a href=\"{$privacyUrl}\">informativa sulla privacy</a> e sui <a href=\"{$cookieUrl}\">cookie</a>, acconsento al trattamento dei miei dati personali",
+                "cb_label2" => "Ho letto e accettato i <a href=\"{$termsUrl}\">termini e condizioni</a> del servizio",
                 "subscribe_text" => "Iscriviti","lang_code" => "it"
             ];
         }//if($lang == Langs::Italian){
         else if($lang == Langs::Espanol){
             $values = [
                 "title" => "Boletín informativo","ea_title" => "Correo electrónico",
-                "cb_label1" => "Declaro haber leído la información sobre privacidad y cookies, acepto el tratamiento de mis datos personales",
-                "cb_label2" => "He leído y acepto los Términos y condiciones del servicio",
+                "cb_label1" => "Declaro haber leído la <a href=\"{$privacyUrl}\">información sobre privacidad</a> y <a href=\"{$cookieUrl}\">cookies</a>, acepto el tratamiento de mis datos personales",
+                "cb_label2" => "He leído y acepto los <a href=\"{$termsUrl}\">términos y condiciones</a> del servicio",
                 "subscribe_text" => "Subscribir","lang_code" => "es"
             ];
         }//else if($lang == Langs::Espanol){
         else{
            $values = [
             "title" => "Newsletter","ea_title" => "Email address",
-            "cb_label1" => "I declare that I have read the information on privacy and cookies, I agree to the processing of my personal data",
-            "cb_label2" => "I have read and agreed with the Terms and conditions of the service",
+            "cb_label1" => "I declare that I have read the <a href=\"{$privacyUrl}\">information on privacy</a> and <a href=\"{$cookieUrl}\">cookies</a>, I agree to the processing of my personal data",
+            "cb_label2" => "I have read and agreed with the <a href=\"{$termsUrl}\">terms and conditions</a> of the service",
             "subscribe_text" => "Subscribe","lang_code" => "en"
            ]; 
         }
