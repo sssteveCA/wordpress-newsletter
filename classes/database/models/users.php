9ca04b1be11f596622bfd7cf72aa997d13227abc
Acer <Acer@DESKTOP-K09QF87>
2022-10-07 19:01:17 +0200

    Added traits/userstrait.php

--- classes/database/models/users.php
+++ classes/database/models/users.php
@@ -6,10 +6,11 @@ use Newsletter\Classes\Database\Models;
 use Newsletter\Classes\Database\Models\UsersErrors as Ue;
 use Newsletter\Exceptions\IncorrectVariableFormatException;
 use Newsletter\Traits\UserCommonTrait;
+use Newsletter\Traits\UsersTrait;
 
 class Users extends Models implements Ue{
 
-    use UserCommonTrait;
+    use UserCommonTrait, UsersTrait;
 
     public function __construct(array $data)
     {
@@ -49,42 +50,6 @@ class Users extends Models implements Ue{
     }
 
     /**
-     * Set the array for wpdb delete function
-     */
-    private function setDeleteArray(array $where): array|null{
-        $this->errno = 0;
-        $classname = __CLASS__;
-        $query = [
-            "where" => [],
-            "where_format" => []
-        ];
-        foreach($where as $field => $val){
-            if(in_array($field, $classname::$fields)){
-                array_push($query["where"],[$field => $val]);
-                switch($field){
-                    case $classname::$fields['id']: //Id
-                    case $classname::$fields['subscribed']: //subscribed
-                        array_push($query["where_format"],"%d");
-                        break;
-                    case $classname::$fields['firstName']:
-                    case $classname::$fields['lastName']:
-                    case $classname::$fields['email']:
-                    case $classname::$fields['verCode']:
-                    case $classname::$fields['unsubscCode']:
-                    case $classname::$fields['subscDate']:
-                    case $classname::$fields['actDate']:
-                        array_push($query["where_format"],"%s");
-                        break;
-                    default:
-                        break;
-                }
-            }//if(in_array($field, Users::$fields)){
-            else throw new IncorrectVariableFormatException(Ue::EXC_INVALID_FIELD);
-        }//foreach($where as $field => $val){
-        return $query;
-    }
-
-    /**
      * Get multiple users from the users table
      * @param array $where the select filter
      * @return array|null array of User objects or null if error
@@ -108,42 +73,6 @@ class Users extends Models implements Ue{
     }
 
     /**
-     * Set the array for the SELECT query to get multiple users
-     */
-    private function setSelectQuery(array $where): array|null{
-        $this->errno = 0;
-        $classname = __CLASS__;
-        $selectArray = [
-            "query" => "",
-            "values" => []
-        ];
-        if(count($where) < 0){
-            //Select all users
-            return $selectArray;
-        }//if(count($where) < 0){
-        else{
-            $selectArray['query'] .= "WHERE ";
-            foreach($where as $field => $val){
-                if(in_array($field, $classname::$fields)){
-                    $selectArray['query'] .= " `{$field}` =";
-                    if(in_array($field, array($classname::$fields['id'], $classname::$fields['subscribed']))){
-                        $selectArray['query'] .= " %d ";
-                    }
-                    else{
-                        $selectArray['query'] .= " %s ";
-                    } 
-                    array_push($selectArray["values"],$val);
-                    if($field != array_key_last($where)) $selectArray['query'] .= ",";
-                    else $selectArray['query'] .= ";";
-                }//if(in_array($field, Users::$fields)){
-                else throw new IncorrectVariableFormatException(Ue::EXC_INVALID_FIELD);
-            }
-            return $selectArray;
-        }//else di if(count($where) < 0){
-        return null;
-    }
-
-    /**
      * Insert multiple users in database
      * @param array $users the array of User objects
      * @param bool $withNames if true the query insert also 'firstName' & 'lastName' fields
@@ -154,71 +83,6 @@ class Users extends Models implements Ue{
         return false;
     }
 
-    /**
-     * Set the array for the INSERT query to insert multiple users
-     * @param array $users the array of User objects
-     * @param bool $withNames if true the query insert also 'firstName' & 'lastName' fields
-     * @return array the array ready for sanitize
-     */
-    private function setInsertArray(array $users, bool $withNames): array|null{
-        $this->errno = 0;
-        $classname = __CLASS__;
-        if(!empty($users)){
-            $insertArray = [
-                "query" => "",
-                "values" => []
-            ];
-            $insertArray["query"] .= $this->setFirstPartInsertSql($withNames);
-            foreach($users as $user){
-                if($user instanceof User){
-                    if($this->validateUserData($user,$withNames)){
-                        if($withNames){
-                            $insertArray["query"] .= "(%s,%s,%s,%s,0,{})";
-                            $userValues = [];
-                            array_push($values);
-                        }
-                        
-                    }//if($this->validateUserData($user,$withNames)){
-                    else throw new IncorrectVariableFormatException(Ue::EXC_DATA_MISSED);
-                }//if($user instanceof User){
-                else throw new IncorrectVariableFormatException(Ue::EXC_INVALID_USERSARRAY);
-            }
-        }//if(!empty($users)){
-        return null;
-    }
-
-    /**
-     * Set the first part of INSERT query for users
-     * @param bool $withNames if true the query insert also 'firstName' & 'lastName' fields
-     * @return string the first part of the INSERT query
-     */
-    private function setFirstPartInsertSql(bool $withNames): string{
-        $classname = __CLASS__;
-        if($withNames){
-            $first_part = <<<SQL
-INSERT INTO `{$this->fullTableName}` (`{$classname::$fields['firstName']}`,`{$classname::$fields['lastName']}`,`{$classname::$fields['email']}`,`{$classname::$fields['verCode']}`,`{$classname::$fields['subscribed']}`,`{$classname::$fields['subscDate']}`) VALUES
-SQL;
-        }
-        else{
-            $first_part = <<<SQL
-INSERT INTO `{$this->fullTableName}` (`{$classname::$fields['email']}`,`{$classname::$fields['verCode']}`,`{$classname::$fields['subscribed']}`,`{$classname::$fields['subscDate']}`) VALUES
-SQL;
-        }
-        return $first_part;
-    }
-
-    /**
-     * Check if User object contains all the required data for insert
-     */
-    private function validateUserData(User $user, bool $withNames): bool{
-        if($user->getEmail() == null) return false;
-        if($user->getLang() == null) return false;
-        if($user->getVerCode() == null) return false;
-        if(!$withNames) return true;
-        if($user->getFirstName() == null) return false;
-        if($user->getLastName() == null) return false;
-        return true;
-    }
 }
 
 interface UsersErrors{
---
+++ traits/userstrait.php
@@ -0,0 +1,153 @@
+<?php
+
+namespace Newsletter\Traits;
+
+use Newsletter\Classes\Database\Models\User;
+use Newsletter\Exceptions\IncorrectVariableFormatException;
+use Newsletter\Classes\Database\Models\UsersErrors as Ue;
+
+/**
+ * Secondary methods used by Users class
+ */
+trait UsersTrait{
+
+    /**
+     * Set the array for wpdb delete function
+     */
+    private function setDeleteArray(array $where): array|null{
+        $this->errno = 0;
+        $classname = __CLASS__;
+        $query = [
+            "where" => [],
+            "where_format" => []
+        ];
+        foreach($where as $field => $val){
+            if(in_array($field, $classname::$fields)){
+                array_push($query["where"],[$field => $val]);
+                switch($field){
+                    case $classname::$fields['id']: //Id
+                    case $classname::$fields['subscribed']: //subscribed
+                        array_push($query["where_format"],"%d");
+                        break;
+                    case $classname::$fields['firstName']:
+                    case $classname::$fields['lastName']:
+                    case $classname::$fields['email']:
+                    case $classname::$fields['verCode']:
+                    case $classname::$fields['unsubscCode']:
+                    case $classname::$fields['subscDate']:
+                    case $classname::$fields['actDate']:
+                        array_push($query["where_format"],"%s");
+                        break;
+                    default:
+                        break;
+                }
+            }//if(in_array($field, Users::$fields)){
+            else throw new IncorrectVariableFormatException(Ue::EXC_INVALID_FIELD);
+        }//foreach($where as $field => $val){
+        return $query;
+    }
+
+    /**
+     * Set the array for the SELECT query to get multiple users
+     */
+    private function setSelectQuery(array $where): array|null{
+        $this->errno = 0;
+        $classname = __CLASS__;
+        $selectArray = [
+            "query" => "",
+            "values" => []
+        ];
+        if(count($where) < 0){
+            //Select all users
+            return $selectArray;
+        }//if(count($where) < 0){
+        else{
+            $selectArray['query'] .= "WHERE ";
+            foreach($where as $field => $val){
+                if(in_array($field, $classname::$fields)){
+                    $selectArray['query'] .= " `{$field}` =";
+                    if(in_array($field, array($classname::$fields['id'], $classname::$fields['subscribed']))){
+                        $selectArray['query'] .= " %d ";
+                    }
+                    else{
+                        $selectArray['query'] .= " %s ";
+                    } 
+                    array_push($selectArray["values"],$val);
+                    if($field != array_key_last($where)) $selectArray['query'] .= ",";
+                    else $selectArray['query'] .= ";";
+                }//if(in_array($field, Users::$fields)){
+                else throw new IncorrectVariableFormatException(Ue::EXC_INVALID_FIELD);
+            }
+            return $selectArray;
+        }//else di if(count($where) < 0){
+        return null;
+    }
+
+    /**
+     * Set the array for the INSERT query to insert multiple users
+     * @param array $users the array of User objects
+     * @param bool $withNames if true the query insert also 'firstName' & 'lastName' fields
+     * @return array the array ready for sanitize
+     */
+    private function setInsertArray(array $users, bool $withNames): array|null{
+        $this->errno = 0;
+        $classname = __CLASS__;
+        if(!empty($users)){
+            $insertArray = [
+                "query" => "",
+                "values" => []
+            ];
+            $insertArray["query"] .= $this->setFirstPartInsertSql($withNames);
+            foreach($users as $user){
+                if($user instanceof User){
+                    if($this->validateUserData($user,$withNames)){
+                        if($withNames){
+                            $insertArray["query"] .= "(%s,%s,%s,%s,0,{})";
+                            $userValues = [];
+                            array_push($values);
+                        }
+                        
+                    }//if($this->validateUserData($user,$withNames)){
+                    else throw new IncorrectVariableFormatException(Ue::EXC_DATA_MISSED);
+                }//if($user instanceof User){
+                else throw new IncorrectVariableFormatException(Ue::EXC_INVALID_USERSARRAY);
+            }
+        }//if(!empty($users)){
+        return null;
+    }
+
+    /**
+     * Set the first part of INSERT query for users
+     * @param bool $withNames if true the query insert also 'firstName' & 'lastName' fields
+     * @return string the first part of the INSERT query
+     */
+    private function setFirstPartInsertSql(bool $withNames): string{
+        $classname = __CLASS__;
+        if($withNames){
+            $first_part = <<<SQL
+INSERT INTO `{$this->fullTableName}` (`{$classname::$fields['firstName']}`,`{$classname::$fields['lastName']}`,`{$classname::$fields['email']}`,`{$classname::$fields['verCode']}`,`{$classname::$fields['subscribed']}`,`{$classname::$fields['subscDate']}`) VALUES
+SQL;
+        }
+        else{
+            $first_part = <<<SQL
+INSERT INTO `{$this->fullTableName}` (`{$classname::$fields['email']}`,`{$classname::$fields['verCode']}`,`{$classname::$fields['subscribed']}`,`{$classname::$fields['subscDate']}`) VALUES
+SQL;
+        }
+        return $first_part;
+    }
+
+    /**
+     * Check if User object contains all the required data for insert
+     */
+    private function validateUserData(User $user, bool $withNames): bool{
+        if($user->getEmail() == null) return false;
+        if($user->getLang() == null) return false;
+        if($user->getVerCode() == null) return false;
+        if(!$withNames) return true;
+        if($user->getFirstName() == null) return false;
+        if($user->getLastName() == null) return false;
+        return true;
+    }
+
+}
+?>
