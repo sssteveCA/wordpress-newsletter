928978ea0cc955fb39c37e82dc1d2c011776f9a4
Acer <Acer@DESKTOP-K09QF87>
2022-10-14 12:52:14 +0200

    Added getUpdateSetString and getUpdateWhereString methods in modeltrait.php

--- traits/modeltrait.php
+++ traits/modeltrait.php
@@ -7,5 +7,47 @@ namespace Newsletter\Traits;
  */
 trait ModelTrait{
 
+    /**
+     * Get the update query SET data ready for prepare
+     */
+    private function getUpdateSetString(array $setArray): array{
+        $sets = "";
+        $values = [];
+        foreach($setArray as $col => $val){
+            if(is_numeric($val))$format = "%d";
+            else $format = "%s";
+            $sets .= "`{$col}` = {$format}"; 
+            array_push($values,$val);
+            if($val != end($set)){
+                //If is not last loop
+                $sets .= ",";
+            }
+        }
+        return [
+            'string' => $sets,
+            'values' => $values
+        ];
+    }
+
+    /**
+     * Get the update query WHERE data ready for prepare
+     */
+    private function getUpdateWhereString(array $whereArray): array{
+        $wheres = "WHERE ";
+        $values = [];
+        foreach($whereArray as $col => $val){
+            if(is_numeric($val))$format = "%d";
+            else $format = "%s";
+            $wheres .= "`{$col}` = {$format} ";
+            array_push($values, $val);
+            if($val != end($where)){
+                $wheres .= "AND ";
+            }
+        }//foreach($where as $col => $val){
+        return [
+            'string' => $wheres,
+            'values' => $values
+        ];
+    }
 }
 ?>
