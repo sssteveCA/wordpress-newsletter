6fb7454dc0c98850a4613a3f125f1f528ee32cc9
Acer <Acer@DESKTOP-K09QF87>
2022-10-21 22:31:36 +0200

    Added deleteUser method in userunsubscribe.php

--- classes/subscribe/userunsubscribe.php
+++ classes/subscribe/userunsubscribe.php
@@ -10,7 +10,11 @@ use Newsletter\Exceptions\IncorrectVariableFormatException;
 use Newsletter\Traits\ErrorTrait;
 
 interface UserUnsubscribeErrors extends ExceptionMessages{
+    const CODE_NOT_FOUND = 1;
+    const FROM_USER = 2;
 
+    const CODE_NOT_FOUND_MSG = "Il codice di disiscrizione inserito non è stato trovato";
+    const FROM_USER_MSG = "Errore dall'oggetto User";
 }
 
 class UserUnsubscribe implements Uue{
@@ -22,9 +26,24 @@ class UserUnsubscribe implements Uue{
     public function __construct(array $data)
     {
         $this->checkValues($data);
+        $this->deleteUser();
     }
 
     public function getUser(){return $this->user;}
+    public function getError(){
+        switch($this->errno){
+            case Uue::CODE_NOT_FOUND:
+                $this->error = Uue::CODE_NOT_FOUND_MSG;
+                break;
+            case Uue::FROM_USER:
+                $this->error = Uue::FROM_USER_MSG;
+                break;
+            default:
+                $this->error = null;
+                break;
+        }
+        return $this->error;
+    }
 
     private function checkValues(array $data){
         if(!isset($data['user'])) throw new NotSettedException(Uue::EXC_NOTISSET);
@@ -37,6 +56,21 @@ class UserUnsubscribe implements Uue{
         $sql = "WHERE `".User::$fields["unsubscCode"]."` = %s";
         $values = [$this->user->getUnsubscCode()];
         return $user_cloned->getUser($sql,$values);
+    }
+
+    private function deleteUser(): bool{
+        if(!$this->checkUnsubscribeCode()){
+            $this->errno = Uue::CODE_NOT_FOUND;
+            return false;
+        }
+        $sql = "WHERE `".User::$fields["unsubscCode"]."` = %s";
+        $values = [$this->user->getUnsubscCode()];
+        $this->user->deleteUser($sql,$values);
+        if($this->user->getErrno() != 0){
+            $this->errno = Uue::FROM_USER;
+            return false;
+        }
+        return true;
     }
 }
 ?>
