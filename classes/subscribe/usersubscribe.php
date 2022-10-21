59945779cd96be8097c573a17723054fbccc86e7
Acer <Acer@DESKTOP-K09QF87>
2022-10-21 19:31:45 +0200

    Added insertUser method in usersubscribe.php

--- classes/subscribe/usersubscribe.php
+++ classes/subscribe/usersubscribe.php
@@ -6,8 +6,21 @@ use Newsletter\Classes\Database\Models\User;
 use Newsletter\Classes\Properties;
 use Newsletter\Exceptions\IncorrectVariableFormatException;
 use Newsletter\Exceptions\NotSettedException;
+use Newsletter\Interfaces\ExceptionMessages;
+use Newsletter\Classes\Subscribe\UserSubscribeError as Usee;
+use Newsletter\Traits\ErrorTrait;
 
-class UserSubscribe{
+interface UserSubscribeError extends ExceptionMessages{
+    const FROM_USER = 1;
+    const INCORRECT_EMAIL = 2;
+
+    const FROM_USER_MSG = "Errore dall'oggetto User";
+    const INCORRECT_EMAIL_MSG = "L' indirizzo email inserito non è in un formato valido";
+}
+
+class UserSubscribe implements Usee{
+
+    use ErrorTrait;
 
     private ?User $user;
     private string $userLang;
@@ -19,11 +32,24 @@ class UserSubscribe{
 
     public function __construct(array $data)
     {
-        
+        $this->checkValues($data);
     }
 
     public function getUser(){return $this->user;}
     public function getUserLang(){return $this->userLang;}
+    public function getError(){
+        switch($this->errno){
+            case Usee::FROM_USER:
+                $this->error = Usee::FROM_USER_MSG;
+                break;
+            case Usee::INCORRECT_EMAIL:
+                $this->error = Usee::INCORRECT_EMAIL_MSG;
+                break;
+            default:
+                $this->error = null;
+                break;
+        }
+    }
 
     private function checkValues(array $data){
         if(!isset($data['lang'])) $data['lang'] = 'en';
@@ -37,6 +63,18 @@ class UserSubscribe{
             throw new IncorrectVariableFormatException($message);
         }
         $this->user = $data['user'];
+    }
+
+    private function insertUser(): bool{
+        $this->errno = 0;
+        $email = $this->user->getEmail();
+        if(preg_match(UserSubscribe::$regex["email"],$email)){
+            $insert = $this->user->insertUser();
+            if($insert) return true;
+            else $this->errno = Usee::FROM_USER;
+        }
+        else $this->errno = Usee::INCORRECT_EMAIL;
+        return false;
     }
 }
 ?>
