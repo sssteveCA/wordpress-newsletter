8044bf471f33813c81760da50a66535f542af12b
Acer <Acer@DESKTOP-K09QF87>
2022-10-28 21:55:15 +0200

    Added activationMailTitle and clickActivationLink methods in activationmailtrait.php

--- traits/properties/messages/activationmailtrait.php
+++ traits/properties/messages/activationmailtrait.php
@@ -2,7 +2,43 @@
 
 namespace Newsletter\Traits\Properties\Messages;
 
+use Newsletter\Enums\Langs;
+
 trait ActivationMailTrait{
-    
+
+    /**
+     * Get the activation mail <title> tag content
+     * @param string $lang the user language
+     * @return  string the activation mail <title> tag content
+     */
+    public static function activationMailTitle(string $lang): string{
+        if($lang == Langs::$langs["it"]){
+            return "Iscrizione Newsletter";
+        }
+        else if($lang == Langs::$langs["es"]){
+            return "Suscripción al boletín informativo";
+        }
+        else{
+            return "Newsletter subscription"; 
+        }
+    }
+
+    /**
+     * Get the activation mail click link message
+     * @param string $lang the user language
+     * @param string $link the user activation URL
+     * @return string the activation mail click link message
+     */
+    public static function clickActivationLink(string $lang, string $link): string{
+        if($lang == Langs::$langs["it"]){
+            return "Per completare l'iscrizione alla newsletter fare click a <a href=\"{$link}\">questo link</a>";
+        }
+        else if($lang == Langs::$langs["es"]){
+            return "Para completar la suscripción al boletín, haga clic en <a href=\"{$link}\">este enlace</a>";
+        }
+        else{
+            return "To complete the newsletter subscription click on <a href=\"{$link}\">this link</a>";
+        }
+    }
 }
 ?>
