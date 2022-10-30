5188e160676a0c326114f49ad568ce39c112cf47
Acer <Acer@DESKTOP-K09QF87>
2022-10-30 09:12:58 +0100

    Added activationMailMessages method in activationmailtrait.php

--- traits/properties/messages/activationmailtrait.php
+++ traits/properties/messages/activationmailtrait.php
@@ -87,5 +87,51 @@ HTML;
             return "For more information <a href=\"".$contactUrl."\">contact us</a>";
         }
     }
+
+    /**
+     * Get the messages used in activation mail template in user language
+     * @param string $lang the user language
+     * @param array $params an array of values, used in the mail
+     * @return array an array containing the message used in activation mail in the user language
+     */
+    public static  function activationMailMessages(string $lang, array $params):array {
+        $code = $params['code'];
+        $contactUrl = $params['contactUrl'];
+        $link = $params['link'];
+        $verifyUrl = $params['verifyUrl'];
+        if($lang == Langs::$langs["it"]){
+            return [
+                "codeWithLink" => <<<HTML
+O in alternativa fai click su <a href="{$verifyUrl}">questo link</a><br>
+E incolla questo codice: {$code}
+HTML,
+                "link" => "Per completare l'iscrizione alla newsletter fare click a <a href=\"{$link}\">questo link</a>",
+                "moreInfo" => "Per maggiori informazioni <a href=\"".$contactUrl."\">contattaci</a>",
+                "title" => "Iscrizione Newsletter"
+            ];
+        }
+        else if($lang == Langs::$langs["es"]){
+            return [
+                "codeWithLink" => <<<HTML
+O, alternativamente, haga clic en <a href="{$verifyUrl}">este enlace</a><br>
+Y pega este código: {$code}
+HTML,
+                "link" => "Para completar la suscripción al boletín, haga clic en <a href=\"{$link}\">este enlace</a>",
+                "moreInfo" => "Para más información <a href=\"".$contactUrl."\">contáctenos</a>",
+                "title" => "Suscripción al boletín informativo"
+            ];
+        }
+        else{
+            return [
+                "codeWithLink" => <<<HTML
+Or alternatively click on <a href="{$verifyUrl}">this link</a><br>
+And paste this code: {$code}
+HTML,
+                "link" => "To complete the newsletter subscription click on <a href=\"{$link}\">this link</a>",
+                "moreInfo" => "For more information <a href=\"".$contactUrl."\">contact us</a>",
+                "title" => "Newsletter subscription"
+            ];
+        }
+    }
 }
 ?>
