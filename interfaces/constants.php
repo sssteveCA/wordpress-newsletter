e1cfe98d9c92da642c4e0081f69266d0c1f03b24
Acer <Acer@DESKTOP-K09QF87>
2022-10-30 18:09:46 +0100

    Added scss/wp/nl_wp.scss

--- interfaces/constants.php
+++ interfaces/constants.php
@@ -8,9 +8,10 @@ interface Constants{
     const FILE_LOG = 'log.txt';
     
     //Paths
-    const REL_CSS_ADMIN = 'css/nl_admin.css';
-    const REL_JS_ADMIN = 'js/nl_admin.js';
-
+    const REL_CSS_ADMIN = 'css/admin/nl_admin.css';
+    const REL_CSS_WP = 'css/wp/nl_wp.css';
+    const REL_JS_ADMIN = 'js/admin/nl_admin.js';
+    const REL_JS_WP = 'js/admin/nl_wp.js';
     const TABLE_USERS = "newsletter_users";
 }
 ?>
