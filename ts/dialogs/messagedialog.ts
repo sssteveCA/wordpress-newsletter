03e4befb2de1f43b6777643702a67fcbdc946435
Acer <Acer@DESKTOP-K09QF87>
2022-11-03 18:52:59 +0100

    Added setDialogHtml method in messagedialog.ts

--- ts/dialogs/messagedialog.ts
+++ ts/dialogs/messagedialog.ts
@@ -15,7 +15,8 @@ export default class MessageDialog{
     private _error: string|null;
 
     constructor(data: BsDialogData){
-
+        this.assignValues(data);
+        this.setDialogHtml();
     }
 
     get title(){return this._title;} 
@@ -41,6 +42,27 @@ export default class MessageDialog{
         if(data.btOkText) this._btOkText = data.btOkText;
         else this._btOkText = "OK";
 
+    }
+
+    private setDialogHtml(): void{
+        this._html = `
+<div id="nl_messagedialog" class="modal" tabindex="-1">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">${this._title}</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="nl_messagedialog" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <p>${this._message}</p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" id="nl_md_ok" class="btn btn-secondary" data-bs-dismiss="modal">${this._btOkText}</button>
+            </div>
+        </div>
+    </div>
+</div>       
+        `;
     }
 
 
