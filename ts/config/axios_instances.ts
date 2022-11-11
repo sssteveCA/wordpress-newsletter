61f53ea8cef30df93c152afd7e106b69c6b7e2c6
Acer <Acer@DESKTOP-K09QF87>
2022-11-11 22:44:19 +0100

    Added clientGet instance to axios_instances.ts

--- ts/config/axios_instances.ts
+++ ts/config/axios_instances.ts
@@ -4,9 +4,15 @@ import { Constants } from '../namespaces/constants.js';
 //import axios from '../../node_modules/axios/index.js';
 declare const axios: AxiosStatic;
 
+export const clientGet = axios.create({
+    baseURL: Constants.HOME_URL,
+    timeout: 10000,
+    responseType: 'text'
+});
+
 export const clientPost = axios.create({
     baseURL: Constants.HOME_URL,
-    timeout: 5000,
+    timeout: 10000,
     headers: {
         'Content-Type' : 'application-json', 'Accept': 'application/json'
     },
