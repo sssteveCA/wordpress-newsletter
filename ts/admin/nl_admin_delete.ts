87602360c91afe2cbcf62043685e9de22b5f9653
Acer <Acer@DESKTOP-K09QF87>
2022-11-15 10:52:03 +0100

    Added checkedEmailsList and emailSelection function calls to nl_admin_delete.ts

--- ts/admin/nl_admin_delete.ts
+++ ts/admin/nl_admin_delete.ts
@@ -1,13 +1,10 @@
-import { GetSubscribersHtml, GetSubscribersHtmlInterface } from "../html/getsubscribershtml";
-import GetSubscribers from "../requests/get_subscribers";
+import { checkedEmailsList, emailSelection } from "../general/admincommon.js";
+import { GetSubscribersHtml, GetSubscribersHtmlInterface } from "../html/getsubscribershtml.js";
+import GetSubscribers from "../requests/get_subscribers.js";
 import { NlFormDataDelete } from "../types/types";
 
 window.addEventListener('DOMContentLoaded',()=>{
     let form: HTMLFormElement = document.getElementById('nl_form_add') as HTMLFormElement;
-    let cb_all: HTMLInputElement = document.getElementById('nl_check_all') as HTMLInputElement;
-    let cb_all_it: HTMLInputElement = document.getElementById('nl_check_all_it') as HTMLInputElement;
-    let cb_all_es: HTMLInputElement = document.getElementById('nl_check_all_es') as HTMLInputElement;
-    let cb_all_en: HTMLInputElement = document.getElementById('nl_check_all_en') as HTMLInputElement;
     let gs: GetSubscribers = new GetSubscribers();
     gs.getSubscribers().then(res => {
         console.log(res);
@@ -15,15 +12,15 @@ window.addEventListener('DOMContentLoaded',()=>{
             containerId: 'nl_del_content_email', subscribers: gs.subscribers
         };
         let gsh: GetSubscribersHtml = new GetSubscribersHtml(gsh_data);
+        emailSelection('nl_del_content_email');
     });
     form.addEventListener('submit', (e)=>{
         e.preventDefault();
-        const data: NlFormDataDelete = {
-            emails: []
-        };
+        let emails: string[] = checkedEmailsList('nl_del_content_email');
+        if(emails.length > 0){
+            const data: NlFormDataDelete = {
+                emails: emails
+            };
+        }//if(emails.length > 0){
     });//form.addEventListener('submit', (e)=>{
-    cb_all.addEventListener('change',()=>{});
-    cb_all_it.addEventListener('change',()=>{});
-    cb_all_es.addEventListener('change',()=>{});
-    cb_all_en.addEventListener('change',()=>{});
 });
