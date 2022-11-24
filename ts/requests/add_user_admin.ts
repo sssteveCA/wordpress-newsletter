f201de93e7dbecceed1d15dfa87a4570542dfab2
Acer <Acer@DESKTOP-K09QF87>
2022-11-24 12:58:29 +0100

    Added ts/requests/add_user_admin.ts

---
+++ ts/requests/add_user_admin.ts
@@ -0,0 +1,36 @@
+import { Constants } from "../namespaces/constants";
+
+export class AddUserAdmin{
+    private _name: string;
+    private _surname: string;
+    private _email: string;
+    private _lang_code: string;
+    private _errno: number = 0;
+    private _error: string|null = null;
+
+    private static NEWUSER_URL:string = Constants.PLUGIN_DIR+"/scripts/subscribe/admin_new_user.php";
+
+    public static ERR_FETCH: number = 1;
+    private static ERR_FETCH_MSG:string = "Errore durante l'esecuzione della richiesta.";
+
+    constructor(){
+
+    }
+
+    get name(){return this._name;}
+    get surname(){return this._surname;}
+    get email(){return this._email;}
+    get lang_code(){return this._lang_code;}
+    get errno(){ return this._errno; }
+    get error(){
+        switch(this._errno){
+            case AddUserAdmin.ERR_FETCH:
+                this._error = AddUserAdmin.ERR_FETCH_MSG;
+                break;
+            default:
+                this._error = null;
+                break;
+        }
+        return this._error;
+    }
+}
