d99b903b71a5d0f1fd6657f3c08f7be8f2895202
stepp <stepp@mail.com>
2022-12-28 23:49:52 +0100

    Added Basic Authorization check to api sendmail.php

--- scripts/api/emailsending/sendmail.php
+++ scripts/api/emailsending/sendmail.php
@@ -1,40 +1,42 @@
 <?php
 
-require_once("../../../../../wp-load.php");
-require_once("../../enums/languages.php");
-require_once("../../interfaces/constants.php");
-require_once("../../interfaces/exceptionmessages.php");
-require_once("../../interfaces/messages.php");
-require_once("../../exceptions/notsettedexception.php");
-require_once("../../vendor/autoload.php");
+require_once("../../../../../../wp-load.php");
+require_once("../../../enums/languages.php");
+require_once("../../../interfaces/constants.php");
+require_once("../../../interfaces/exceptionmessages.php");
+require_once("../../../interfaces/messages.php");
+require_once("../../../exceptions/notsettedexception.php");
+require_once("../../../vendor/autoload.php");
 //require_once("../../traits/properties/messages/activationmailtrait.php");
-require_once("../../traits/properties/messages/newusertrait.php");
-require_once("../../traits/properties/messages/othertrait.php");
-require_once("../../traits/properties/messages/unsubscribetrait.php");
-require_once("../../traits/properties/messages/verifytrait.php");
-require_once("../../traits/properties/propertiesmessagestrait.php");
-require_once("../../traits/properties/propertiesurltrait.php");
+require_once("../../../traits/properties/messages/newusertrait.php");
+require_once("../../../traits/properties/messages/othertrait.php");
+require_once("../../../traits/properties/messages/unsubscribetrait.php");
+require_once("../../../traits/properties/messages/verifytrait.php");
+require_once("../../../traits/properties/propertiesmessagestrait.php");
+require_once("../../../traits/properties/propertiesurltrait.php");
 //require_once("../../traits/properties/propertiesvaluestrait.php");
-require_once("../../traits/emailmanagertrait.php");
-require_once("../../traits/templatetrait.php");
-require_once("../../traits/errortrait.php");
-require_once("../../traits/modeltrait.php");
-require_once("../../traits/sqltrait.php");
-require_once("../../traits/usertrait.php");
-require_once("../../traits/userstrait.php");
-require_once("../../traits/usercommontrait.php");
-require_once("../../classes/properties.php");
-require_once("../../classes/template.php");
-require_once("../../classes/database/tables/table.php");
-require_once("../../classes/database/model.php");
-require_once("../../classes/database/models.php");
-require_once("../../classes/database/models/user.php");
-require_once("../../classes/database/models/users.php");
-require_once("../../classes/email/emailmanager.php");
+require_once("../../../traits/emailmanagertrait.php");
+require_once("../../../traits/templatetrait.php");
+require_once("../../../traits/errortrait.php");
+require_once("../../../traits/modeltrait.php");
+require_once("../../../traits/sqltrait.php");
+require_once("../../../traits/usertrait.php");
+require_once("../../../traits/userstrait.php");
+require_once("../../../traits/usercommontrait.php");
+require_once("../../../classes/properties.php");
+require_once("../../../classes/template.php");
+require_once("../../../classes/database/tables/table.php");
+require_once("../../../classes/database/model.php");
+require_once("../../../classes/database/models.php");
+require_once("../../../classes/database/models/user.php");
+require_once("../../../classes/database/models/users.php");
+require_once("../../../classes/email/emailmanager.php");
+require_once("../../../classes/api/authcheck.php");
 
 use Newsletter\Interfaces\Messages as M;
 use Newsletter\Classes\Email\EmailManagerErrors as Eme;
 use Dotenv\Dotenv;
+use Newsletter\Classes\Api\AuthCheck;
 use Newsletter\Classes\Email\EmailManager;
 use Newsletter\Exceptions\NotSettedException;
 
@@ -42,17 +44,23 @@ $response = [
     'done' => false, 'msg' => ''
 ];
 
-$current_user = wp_get_current_user();
-$logged = ($current_user->ID != 0);
-$administrator = current_user_can('manage_options');
-
-//if($logged && $administrator){
-    $input = file_get_contents("php://input");
-    $post = json_decode($input,true);
-    //$response['data'] = $post;
-    if(isset($post['emails'],$post['subject'],$post['body']) && $post['body'] != ''){
-        if(is_array($post['emails']) && sizeof($post['emails']) > 0){
-            try{
+try{
+    $dotenv = Dotenv::createImmutable("../../../");
+    $dotenv->load();
+    $apiAuthArray = [
+        'username' => $_SERVER['PHP_AUTH_USER'],
+        'password' => $_SERVER['PHP_AUTH_PW'],
+        'uuid' => $_ENV['API_REST_UUID']
+    ];
+    echo "apiAuthArray => \r\n";
+    var_dump($apiAuthArray);
+    $authCheck = new AuthCheck($apiAuthArray);
+    if($authCheck->getErrno() == 0){
+        $input = file_get_contents("php://input");
+        $post = json_decode($input,true);
+        //$response['data'] = $post;
+        if(isset($post['emails'],$post['subject'],$post['body']) && $post['body'] != ''){
+            if(is_array($post['emails']) && sizeof($post['emails']) > 0){
                 $snData = [
                     'body' => $post['body'], 'emails' => $post['emails'], 'subject' => $post['subject']
                 ];
@@ -71,23 +79,30 @@ $administrator = current_user_can('manage_options');
                         $response['msg'] = M::ERR_UNKNOWN;
                         break;          
                 }//switch($emErrno){
-            }catch(NotSettedException $nse){
+            }//if(is_array($post['emails'] && sizeof($post['emails']) > 0)){
+            else{
                 http_response_code(400);
-                $response['msg'] = $nse->getMessage();
-            }catch(Exception $e){
-                http_response_code(500);
-                $response['msg'] = M::ERR_UNKNOWN;
+                $response['msg'] = M::ERR_ATLEAST_ONE_EMAIL;
             }
-        }//if(is_array($post['emails'] && sizeof($post['emails']) > 0)){
+        }//if(isset($post['emails'],$post['subject'],$post['body']) && $post['body'] != ''){
         else{
             http_response_code(400);
-            $response['msg'] = M::ERR_ATLEAST_ONE_EMAIL;
+            $response['msg'] = M::ERR_MISSING_FORM_VALUES;
         }
-    }//if(isset($post['emails'],$post['subject'],$post['body']) && $post['body'] != ''){
+    }//if($authCheck->getErrno() == 0){
     else{
-        http_response_code(400);
-        $response['msg'] = M::ERR_MISSING_FORM_VALUES;
+        http_response_code(401);
+        $response["msg"] = M::ERR_UNAUTHORIZED;
     }
+}catch(NotSettedException $nse){
+    http_response_code(400);
+    $response['msg'] = M::ERR_UNAUTHORIZED;
+}catch(Exception $e){
+    http_response_code(500);
+    $response['msg'] = M::ERR_UNKNOWN;
+}
+    
+
 //}//if($logged && $administrator){
 /* else{
     http_response_code(401);
@@ -97,8 +112,6 @@ $administrator = current_user_can('manage_options');
 echo json_encode($response,JSON_UNESCAPED_SLASHES|JSON_UNESCAPED_UNICODE);
 
 function sendNewsletterMail(array $params):int {
-    $dotEnv = Dotenv::createImmutable("../../");
-    $dotEnv->safeLoad();
     $from = isset($params['from']) ? $params['from'] : $_ENV['EMAIL_USERNAME'];
     $fromNickname = isset($params['fromNickname']) ? $params['fromNickname'] : $_ENV['EMAIL_NICKNAME'];
     $host = isset($params['host']) ? $params['host'] : $_ENV['EMAIL_HOST'];
