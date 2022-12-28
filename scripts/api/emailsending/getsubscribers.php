f4b13a5ae9edd6e7d95d0977f380d24e49e95566
stepp <stepp@mail.com>
2022-12-28 22:37:36 +0100

    Added Basic Authorization check to api/getsubscribers.php

--- scripts/api/emailsending/getsubscribers.php
+++ scripts/api/emailsending/getsubscribers.php
@@ -17,23 +17,34 @@ require_once("../../classes/database/model.php");
 require_once("../../classes/database/models.php");
 require_once("../../classes/database/models/user.php");
 require_once("../../classes/database/models/users.php");
+require_once("../../../classes/api/authcheck.php");
 
 use Newsletter\Classes\Database\Models\Users;
 use Newsletter\Classes\Database\ModelErrors as Me;
 use Newsletter\Classes\Database\Models\UsersErrors as Ue;
 use Newsletter\Interfaces\Constants as C;
 use Newsletter\Interfaces\Messages as M;
+use Dotenv\Dotenv;
+use Newsletter\Classes\Api\AuthCheck;
+use Newsletter\Exceptions\NotSettedException;
 
 $response = [
     'done' => false,'empty' => false, 'msg' => '','subscribers' => [] 
 ];
 
-$current_user = wp_get_current_user();
-$logged = ($current_user->ID != 0);
-$administrator = current_user_can('manage_options');
+$dotenv = Dotenv::createImmutable("../../../");
+$dotenv->load();
+
+$apiAuthArray = [
+    'username' => $_SERVER['PHP_AUTH_USER'],
+    'password' => $_SERVER['PHP_AUTH_PW'],
+    'uuid' => $_ENV['API_REST_UUID']
+];
 
 //if($logged && $administrator){
-    try{
+try{
+    $authCheck = new AuthCheck($apiAuthArray);
+    if($authCheck->getErrno() == 0){
         $users_data = ['tableName' => C::TABLE_USERS];
         $users = new Users($users_data);
         $users_where = ['subscribed' => 1];
@@ -56,17 +67,19 @@ $administrator = current_user_can('manage_options');
                 $response['msg'] = M::ERR_UNKNOWN;
                 break;
         }
-    
-    }catch(Exception $e){
-        //echo "GetSubscribers exception => ".$e->getMessage()."\r\n";
-        http_response_code(500);
-        $response['msg'] = M::ERR_UNKNOWN;
+    }//if($authCheck->getErrno() == 0){
+    else{
+        http_response_code(401);
+        $response['msg'] = M::ERR_UNAUTHORIZED;
     }
-//}//if($logged && $administrator){
-/* else{
+}catch(NotSettedException $nse){
     http_response_code(401);
     $response['msg'] = M::ERR_UNAUTHORIZED;
-} */
+}catch(Exception $e){
+    //echo "GetSubscribers exception => ".$e->getMessage()."\r\n";
+    http_response_code(500);
+    $response['msg'] = M::ERR_UNKNOWN;
+}
 
 echo json_encode($response,JSON_UNESCAPED_SLASHES|JSON_UNESCAPED_UNICODE);
 
