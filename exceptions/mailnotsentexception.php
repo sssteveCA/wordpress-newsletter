23dd0b2910c23346701fcff7ad356c0a1c41e759
stepp <stepp@mail.com>
2023-01-01 22:11:46 +0100

    Added exceptions/mailnotsentexception.php

---
+++ exceptions/mailnotsentexception.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Newsletter\Exceptions;
+
+use Exception;
+use Throwable;
+
+class MailNotSentException extends Exception{
+    public function __construct($message, $code = 0, Throwable $previous = null){
+        parent::__construct($message, $code, $previous);
+    }
+
+    public function __toString(){
+        return __CLASS__.": {{$this->code}}: {{$this->message}}\n";
+    }
+}
+?>
