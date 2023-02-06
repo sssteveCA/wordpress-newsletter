90c33407136514868e42f4f3a5db7b56ae0a1297
stepp <stepp@mail.com>
2023-02-06 11:07:45 +0100

    Added KEY_DONE and KEY_MESSAGE constants to constants.ts.example

--- ts/namespaces/constants.example.ts
+++
@@ -1,6 +0,0 @@
-
-/** Place your values in a file called constants.ts */
-export namespace Constants{
-    export const HOME_URL:string = "";
-    export const PLUGIN_DIR: string = "/wp-content/plugins/newsletter"; 
-}
