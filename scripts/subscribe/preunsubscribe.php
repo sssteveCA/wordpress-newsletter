732cba8e6e33da991c9bb83256a44c5e994e5bea
stepp <stepp@mail.com>
2023-04-04 20:26:53 +0200

    Added ts/scripts/preunsubscribe.ts

--- scripts/subscribe/preunsubscribe.php
+++ scripts/subscribe/preunsubscribe.php
@@ -2,10 +2,17 @@
 use Newsletter\Classes\General;
 use Newsletter\Classes\Properties;
 
+require_once("../../../../../wp-load.php");
 require_once("../../enums/languages.php");
+require_once("../../traits/properties/messages/newusertrait.php");
+require_once("../../traits/properties/messages/othertrait.php");
+require_once("../../traits/properties/messages/verifytrait.php");
+require_once("../../traits/properties/messages/preunsubscribetrait.php");
+require_once("../../traits/properties/messages/unsubscribetrait.php");
 require_once("../../traits/properties/propertiesmessagestrait.php");
 require_once("../../traits/properties/propertiesurltrait.php");
 require_once("../../classes/properties.php");
+require_once("../../classes/general.php");
 require_once("../../classes/htmlcode.php");
 
 if(!isset($_REQUEST['lang'])) $_REQUEST['lang'] = 'en';
@@ -16,12 +23,15 @@ $arrData = [
     'body' => '',
     'styleTag' => '',
     'styles' => [],
-    'scripts' => []
+    'scripts' => [
+        '../../js/scripts/preunsubscribe.js'
+    ]
 ];
 
 if(isset($_REQUEST["unsubscCode"]) && $_REQUEST["unsubscCode"]){
+    $script_path = "./unsubscribe.php";
     $unsubsc_code = $_REQUEST["unsubscCode"];
-    $arr_data['body'] = '';
+    $arr_data['body'] = Properties::preUnsubscribeForm($lang,$script_path,$unsubsc_code);
 }//if(isset($_REQUEST["unsubscCode"]) && $_REQUEST["unsubscCode"]){
 else{
     $error_message = Properties::preUnsubscribeErrorMessage($lang);
