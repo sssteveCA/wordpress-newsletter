8428988efc2c2b0d7f8e07aeb84f8edd8f640b3f
stepp <stepp@mail.com>
2023-04-04 21:26:30 +0200

    Added ts/requests/unsubscribe_user.ts

---
+++ ts/requests/unsubscribe_user.ts
@@ -0,0 +1,40 @@
+import { NlUnsubscribeUserData } from "../types/types";
+import { Languages } from "../enums/enums";
+import { Constants } from "../namespaces/constants";
+
+export class UnsubscribeUser{
+
+    private _lang: string;
+    private _unsubscribe_code: string;
+    private _errno: number = 0;
+    private _error: string|null = null;
+
+    public static ERR_FETCH: number = 1;
+
+    private static ERR_FETCH_MSG: string = "Errore durante la disiscrizione. Se il problema persiste contattare l'amministratore del sito.";
+
+    private static FETCH_URL: string = Constants.PLUGIN_DIR+"/scripts/subscribe/delete_user.php";
+
+    constructor(data: NlUnsubscribeUserData){
+        if(data.lang)
+            this._lang = data.lang;
+        else
+            this._lang = Languages.EN;
+        this._unsubscribe_code = data.unsubscribe_code;
+    }
+
+    get lang(){ return this._lang}
+    get unsubscribe_code(){ return this._unsubscribe_code}
+    get errno(){ return this._errno; }
+    get error(){
+        switch(this._errno){
+            case UnsubscribeUser.ERR_FETCH:
+                this._error = UnsubscribeUser.ERR_FETCH_MSG;
+                break;
+            default:
+                this._error = null;
+                break;
+        }
+        return this._error;
+    }
+}
--- ts/scripts/preunsubscribe.ts
+++ ts/scripts/preunsubscribe.ts
@@ -1,4 +1,7 @@
 
 window.addEventListener('DOMContentLoaded',()=>{
-    
+    let form = document.getElementById('fUnsubscribe') as HTMLFormElement;
+    form.addEventListener('submit',(e)=>{
+        e.preventDefault();
+    })
 })
