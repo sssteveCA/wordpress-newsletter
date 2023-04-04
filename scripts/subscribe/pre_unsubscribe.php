ec20c71dc24bd739e6adc04add3892c0ab5ffcca
stepp <stepp@mail.com>
2023-04-04 19:03:56 +0200

    Added preUnsubscribeTitle method to unsubscribetrait.php

--- scripts/subscribe/pre_unsubscribe.php
+++ scripts/subscribe/pre_unsubscribe.php
@@ -1,3 +1,27 @@
 <?php
+use Newsletter\Classes\General;
 
+require_once("../../enums/languages.php");
+require_once("../../traits/properties/propertiesmessagestrait.php");
+require_once("../../traits/properties/propertiesurltrait.php");
+require_once("../../classes/properties.php");
+require_once("../../classes/htmlcode.php");
+
+if(!isset($_REQUEST['lang'])) $_REQUEST['lang'] = 'en';
+$lang = General::languageCode($_REQUEST['lang']);
+
+$arrData = [
+    'title' => 'Conferma di',
+    'body' => '',
+    'styleTag' => '',
+    'styles' => [],
+    'scripts' => []
+];
+
+if(isset($_REQUEST["unsubscCode"]) && $_REQUEST["unsubscCode"]){
+
+}//if(isset($_REQUEST["unsubscCode"]) && $_REQUEST["unsubscCode"]){
+else{
+
+}
 ?>
--- traits/properties/messages/unsubscribetrait.php
+++ traits/properties/messages/unsubscribetrait.php
@@ -56,5 +56,22 @@ trait UnsubscribeTrait{
             return "You have unsubscribed from the newsletter";
         }
     }
+
+    /**
+     * Get the title of the pre unsubscribe script
+     * @param string $lang the user language
+     * @return string the pre unsubscribe title tag content
+     */
+    public static function preUnsubscribeTitle(string $lang): string{
+        if($lang == Langs::$langs["it"]){
+            return "Conferma disiscrizione dalla newsletter";
+        }
+        else if($lang == Langs::$langs["es"]){
+            return "Confirmar cancelación desde el boletín";
+        }
+        else{
+            return "Confirm newsletter unsubscribe";            
+        }
+    }
 }
 ?>
