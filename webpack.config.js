ea66b7ef4d1c6fbe952b1bc43aa06ac34c39cd57
stepp <stepp@mail.com>
2023-04-10 12:06:26 +0200

    Added entry points to webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,26 @@
 
-const path = require('path')
+const {join,resolve} = require('path')
 
-const srcPath = path.resolve(__dirname,'src')
+const scssPath = join(__dirname,'src','scss')
+const tsPath = join(__dirname,'src','ts')
 
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const FixStyleOnlyEntriesPlugin = require('webpack-fix-style-only-entries')
 
 module.exports = {
     entry: {
-
+        'css/wp/nl_wp': join(scssPath,'wp/nl_wp.scss'),
+        'css/wp/verify': join(scssPath,'wp/verify.scss'),
+        'css/admin/nl_admin_add': join(scssPath,'admin/nl_admin_add.scss'),
+        'css/admin/nl_admin_delete': join(scssPath,'admin/nl_admin_delete.scss'),
+        'css/admin/nl_admin_send': join(scssPath,'admin/nl_admin_send.scss'),
+        'js/wp/nl_wp': join(tsPath,'wp/nl_wp.ts'),
+        'js/admin/nl_admin_add': join(tsPath,'admin/nl_admin_add.ts'),
+        'js/admin/nl_admin_delete': join(tsPath,'admin/nl_admin_delete.ts'),
+        'js/admin/nl_admin_send': join(tsPath,'admin/nl_admin_send.ts'),
     },
     output: {
-        path: path.resolve(__dirname,'dist'),
+        path: resolve(__dirname,'dist'),
         filename: '[name].js',
         clean: true
     },
