eff4233325153d0787c7749c7a1c4ea78ef3f886
stepp <stepp@mail.com>
2023-04-10 11:40:07 +0200

    moved scss and ts folder into src folder

---
+++ src/ts/namespaces/constants.ts
@@ -0,0 +1,10 @@
+
+export namespace Constants{
+    export const HOME_URL:string = "http://localhost/www.lafilosofiadibianca.com";
+    export const PLUGIN_DIR: string = "/wp-content/plugins/newsletter";
+
+    export const KEY_DONE:string = 'done';
+    export const KEY_MESSAGE:string = 'msg';
+
+
+}
