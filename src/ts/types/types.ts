93c8630d620956a937434730f0046f650c897102
stepp <stepp@mail.com>
2023-05-01 18:57:44 +0200

    Added NlFormDataSettingsInputPagesEnabled,NlFormDataSettingsInputLangs and NlFormDataSettingsInputSocial types to types.ts

--- src/ts/types/types.ts
+++ src/ts/types/types.ts
@@ -59,39 +59,33 @@ export type NlFormDataSend = {
 export type NlFormDataSettings = {
     form: HTMLFormElement,
     container_pages_enabled: HTMLDivElement,
-    cb_pages_enabled: {
-        contacts_pages: HTMLInputElement,
-        privacy_policy_pages: HTMLInputElement,
-    },
+    cb_pages_enabled: NlFormDataSettingsInputPagesEnabled,
     container_langs: HTMLDivElement,
-    cb_langs: {
-        lang_it: HTMLInputElement,
-        lang_es: HTMLInputElement,
-        lang_en: HTMLInputElement,
-    },
-    cb_social: {
-        facebook: HTMLInputElement,
-        instagram: HTMLInputElement,
-        youtube: HTMLInputElement,
-    },
+    cb_langs: NlFormDataSettingsInputLangs,
+    cb_social: NlFormDataSettingsInputSocial,
     row_social_links: HTMLDivElement,
-    input_social_links: {
-        facebook: HTMLInputElement,
-        instagram: HTMLInputElement,
-        youtube: HTMLInputElement, 
-    },
+    input_social_links: NlFormDataSettingsInputSocial,
     container_contacts_pages: HTMLDivElement,
-    input_contacts_pages: {
-        lang_it: HTMLInputElement,
-        lang_es: HTMLInputElement,
-        lang_en: HTMLInputElement,
-    },
+    input_contacts_pages: NlFormDataSettingsInputLangs,
     container_privacy_pages: HTMLDivElement,
-    input_privacy_policy_pages: {
-        lang_it: HTMLInputElement,
-        lang_es: HTMLInputElement,
-        lang_en: HTMLInputElement,
-    }
+    input_privacy_policy_pages: NlFormDataSettingsInputLangs
+}
+
+export type NlFormDataSettingsInputPagesEnabled = {
+    contacts_pages: HTMLInputElement,
+    privacy_policy_pages: HTMLInputElement,
+}
+
+export type NlFormDataSettingsInputLangs = {
+    lang_it: HTMLInputElement,
+    lang_es: HTMLInputElement,
+    lang_en: HTMLInputElement,
+}
+
+export type NlFormDataSettingsInputSocial = {
+    facebook: HTMLInputElement,
+    instagram: HTMLInputElement,
+    youtube: HTMLInputElement,
 }
 
 export type NlUnsubscribeUserData = {
