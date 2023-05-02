258d92d2935e1695ebcafb62503bf28175cd00ee
stepp <stepp@mail.com>
2023-05-02 18:41:48 +0200

    Added classes/database/models/settings.php

---
+++ classes/database/models/settings.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Newsletter\Classes\Database\Models;
+use Newsletter\Classes\Database\Models;
+
+class Settings extends Models{
+
+    /**
+     * The languages of the available pages
+     */
+    private array $lang_status = [];
+
+    /**
+     * The inclusion status of the site pages attachable to the newsletter body
+     */
+    private array $included_pages_status = [];
+
+    /**
+     * The social pages link to insert in the newsletter
+     */
+    private array $socials_status = [];
+
+    /**
+     * The social page links to be included in the newsletter
+     */
+    private array $social_pages = [];
+
+    /**
+     * The contact page links in the declared languages
+     */
+    private array $contact_pages = [];
+
+    /**
+     * The privacy policy page links in the declared languages
+     */
+    private array $privacy_policy_pages = [];
+
+    public function __construct(array $data){
+
+    }
+
+    public function getLangStatus(){ return $this->lang_status; }
+    public function getIncludedPagesStatus(){return $this->included_pages_status; }
+    public function getSocialsStatus(){ return $this->socials_status; }
+    public function getSocialPages(){ return $this->social_pages; }
+    public function getContactPages(){ return $this->contact_pages; }
+    public function getPrivacyPolicyPages(){ return $this->privacy_policy_pages; }
+}
+
+?>
