f28381d7a89acb6050c6331964fd8058497fa18c
stepp <stepp@mail.com>
2023-05-02 22:06:08 +0200

    Added assignValues method to models/settings.php

--- classes/database/models/settings.php
+++ classes/database/models/settings.php
@@ -2,41 +2,45 @@
 
 namespace Newsletter\Classes\Database\Models;
 use Newsletter\Classes\Database\Models;
+use Newsletter\Traits\ErrorTrait;
+use Newsletter\Traits\SqlTrait;
 
 class Settings extends Models{
+
+    use ErrorTrait,SqlTrait;
 
     /**
      * The languages of the available pages
      */
-    private array $lang_status = [];
+    private array $lang_status;
 
     /**
      * The inclusion status of the site pages attachable to the newsletter body
      */
-    private array $included_pages_status = [];
+    private array $included_pages_status;
 
     /**
      * The social pages link to insert in the newsletter
      */
-    private array $socials_status = [];
+    private array $socials_status;
 
     /**
      * The social page links to be included in the newsletter
      */
-    private array $social_pages = [];
+    private array $social_pages;
 
     /**
      * The contact page links in the declared languages
      */
-    private array $contact_pages = [];
+    private array $contact_pages;
 
     /**
      * The privacy policy page links in the declared languages
      */
-    private array $privacy_policy_pages = [];
+    private array $privacy_policy_pages;
 
     public function __construct(array $data){
-
+        $this->assignValues($data);
     }
 
     public function getLangStatus(){ return $this->lang_status; }
@@ -45,6 +49,31 @@ class Settings extends Models{
     public function getSocialPages(){ return $this->social_pages; }
     public function getContactPages(){ return $this->contact_pages; }
     public function getPrivacyPolicyPages(){ return $this->privacy_policy_pages; }
+
+    public function getError(){
+        if($this->errno < parent::MAX_MODELS){
+            return parent::getError();
+        }
+        else{
+            switch($this->errno){
+                default:
+                    $this->error = null;
+                    break;
+            }
+        }
+        return $this->error;
+    }
+
+    private function assignValues(array $data){
+        $this->lang_status = isset($data['lang_status']) ? $data['lang_status'] : [];
+        $this->included_pages_status = isset($data['included_pages_status']) ? $data['included_pages_status'] : [];
+        $this->socials_status = isset($data['socials_status']) ? $data['socials_status'] : [];
+        $this->social_pages = isset($data['social_pages']) ? $data['social_pages'] : [];
+        $this->contact_pages = isset($data['contact_pages']) ? $data['contact_pages'] : [];
+        $this->privacy_policy_pages = isset($data['privacy_policy_pages']) ? $data['privacy_policy_pages'] : [];
+    }
+
+
 }
 
 ?>
