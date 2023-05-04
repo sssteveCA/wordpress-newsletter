27bd25e592de890a4cdbad4661fa8603229a25ec
stepp <stepp@mail.com>
2023-05-04 20:24:19 +0200

    Added filterIncludedPagesArray method to settingscheck.php

--- classes/settings/settingscheck.php
+++ classes/settings/settingscheck.php
@@ -4,40 +4,55 @@ namespace Newsletter\Classes\Settings;
 use Newsletter\Traits\ErrorTrait;
 
 /**
- * This class check verify the correctness of the provided user settings data
+ * This class check verify the provided user settings data and eventually correct them
  */
 class SettingsCheck{
     use ErrorTrait;
 
     /**
+     * Keys allowed for included_pages_status property
+     */
+    private static array $included_pages_key_allowed = ["contact_pages","privacy_policy_pages"];
+
+    /**
+     * Keys allowed for lang_status, contact_pages and privacy_policy_pages properties
+     */
+    private static array $language_key_allowed = ["en","es","it"];
+
+    /**
+     * Keys allowed for socials_status and social_pages properties
+     */
+    private static array $socials_key_allowed = ["facebook","instagram","youtube"];
+
+    /**
      * The languages of the available pages
      */
-    private array $lang_status;
+    private array $lang_status = [];
 
     /**
      * The inclusion status of the site pages attachable to the newsletter body
      */
-    private array $included_pages_status;
+    private array $included_pages_status = [];
 
     /**
      * The social pages link to insert in the newsletter
      */
-    private array $socials_status;
+    private array $socials_status = [];
 
     /**
      * The social page links to be included in the newsletter
      */
-    private array $social_pages;
+    private array $social_pages = [];
 
     /**
      * The contact page links in the declared languages
      */
-    private array $contact_pages;
+    private array $contact_pages = [];
 
     /**
      * The privacy policy pages links in the declared languages
      */
-    private array $privacy_policy_pages;
+    private array $privacy_policy_pages = [];
 
     public function __construct(array $data){
 
@@ -49,6 +64,20 @@ class SettingsCheck{
     public function getSocialPages(){ return $this->social_pages; }
     public function getContactPages(){ return $this->contact_pages; }
     public function getPrivacyPolicyPages(){ return $this->privacy_policy_pages; }
+
+    /**
+     * Add only the included page status valid keys and values 
+     */
+    private function filterIncludedPagesArray(array $included_pages_status){
+        foreach($included_pages_status as $key => $value){
+            if(in_array($key,SettingsCheck::$included_pages_key_allowed)){
+                if(in_array($value,[false,true]))
+                    $this->included_pages_status[] = [$key,$value];
+            }
+        }
+    }
+
+
 }
 
 ?>
