4e335b88e36b2fe7c925cd1ac9985f20d537a050
stepp <stepp@mail.com>
2023-06-04 13:15:49 +0200

    Added NewsletterLogManagerErrors interface to newsletterlogmanager.php

--- classes/log/newsletterlogmanager.php
+++ classes/log/newsletterlogmanager.php
@@ -1,17 +1,35 @@
 <?php
 
 namespace Newsletter\Classes\Log;
+
 use Newsletter\Traits\ErrorTrait;
+use Newsletter\Classes\Log\NewsletterLogManagerErrors as Nlme;
+
+interface NewsletterLogManagerErrors{
+    const ERR_FILE_NOT_FOUND = 1;
+
+    const ERR_FILE_NOT_FOUND_MSG = "Impossibile aprire il file di log";
+}
 
 /**
  * Class to retrieve the newsletter log info file
  */
-class NewsletterLogManager{
+class NewsletterLogManager implements Nlme{
 
     use ErrorTrait;
 
     private array $loginfo = [];
 
     public function getLogInfo(){ return $this->loginfo; }
+    public function getError(){
+        switch($this->errno){
+            case Nlme::ERR_FILE_NOT_FOUND:
+                $this->error = Nlme::ERR_FILE_NOT_FOUND_MSG;
+                break;
+            default:
+                $this->error = null;
+        }
+        return $this->error;
+    }
 }
 ?>
