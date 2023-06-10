8ade196a2a1ec96a9f24ecc4f424e91c12f76e6f
stepp <stepp@mail.com>
2023-06-10 20:50:52 +0200

    Added setHtml method to getnewsletterloghtml.ts

--- src/ts/html/getnewsletterloghtml.ts
+++ src/ts/html/getnewsletterloghtml.ts
@@ -10,11 +10,38 @@ export default class GetNewsletterLogHtml{
     constructor(data: NlGetNewsletterLogHtml){
         this._loginfo = data.log_info;
         this._table_container = data.table_container;
+        this.setHtml();
     }
 
     get loginfo(){ return this._loginfo; }
     get errno(){ return this._errno; }
     get error(){ return this._error; }
+
+    private setHtml(): void{
+        let html: string = `
+<table class="table table-striped">
+    <thead>
+        <tr>
+            <th scope="col">OGGETTO</th>
+            <th scope="col">DESTINATARIO</th>
+            <th scope="col">DATA</th>
+        </tr>
+    </thead>
+    <tbody>
+        `;
+        html += this._loginfo.reduce((accumulator,currentValue) => {
+            return `${accumulator}
+        <tr>
+            <td>${currentValue.subject}</td><td>${currentValue.recipient}</td><td>${currentValue.date}</td>
+        </tr>     
+            `;
+        },'')
+        html += `
+    </tbody>
+</table>
+        `;
+        this._table_container.innerHTML = html;
+    }
 
 
 
