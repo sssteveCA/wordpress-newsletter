cb29939661343476975a32cc1375083f582b15ea
stepp <stepp@mail.com>
2023-06-11 11:15:31 +0200

    Added writeFile method to newsletterlogmanager.php

--- classes/log/newsletterlogmanager.php
+++ classes/log/newsletterlogmanager.php
@@ -10,10 +10,12 @@ interface NewsletterLogManagerErrors{
     const ERR_INVALID_FILE = 1;
     const ERR_READ_FILE = 2;
     const ERR_DELETE_FILE = 3;
+    const ERR_WRITE_FILE = 4;
 
     const ERR_INVALID_FILE_MSG = "Il percorso indicato non contiene un file valido";
     const ERR_READ_FILE_MSG = "Impossibile leggere il file";
     const ERR_DELETE_FILE_MSG = "Impossibile cancellare il file";
+    const ERR_WRITE_FILE_MSG = "Impossibile scrivere sul file";
 }
 
 /**
@@ -45,22 +47,30 @@ class NewsletterLogManager implements Nlme{
             case Nlme::ERR_DELETE_FILE:
                 $this->error = Nlme::ERR_DELETE_FILE_MSG;
                 break;
+            case Nlme::ERR_WRITE_FILE:
+                $this->error = Nlme::ERR_WRITE_FILE_MSG;
+                break;
             default:
                 $this->error = null;
+                break;
         }
         return $this->error;
     }
 
     /**
      * Delete the newsletter log file
+     * @return bool
      */
-    public function deleteFile(){
+    public function deleteFile(): bool{
         $this->errno = 0;
         if(file_exists($this->file_path) && is_file($this->file_path)){
             $delete = unlink($this->file_path);
-            if(!$delete) $this->errno = Nlme::ERR_DELETE_FILE;
+            if($delete) return true;
+            $this->errno = Nlme::ERR_DELETE_FILE;
+            return false;
         }//if(file_exists($this->file_path) && is_file($this->file_path)){
-        else $this->errno = Nlme::ERR_INVALID_FILE;
+        $this->errno = Nlme::ERR_INVALID_FILE;
+        return false;
     }
 
     /**
@@ -69,7 +79,7 @@ class NewsletterLogManager implements Nlme{
      */
     public function getLogInfoAssociative(): array{
         $log_info_array = [];
-        foreach($this->getLogInfo() as $item){
+        foreach($this->loginfo as $item){
             $log_info_array[] = [
                 'subject' => $item->getSubject(), 'recipient' => $item->getRecipient(), 'date' => $item->getDate()
             ];
@@ -79,8 +89,9 @@ class NewsletterLogManager implements Nlme{
 
     /**
      * Fetch the newsletter log information from the file
+     * @return  bool
      */
-    public function readFile(){
+    public function readFile(): bool{
         $this->errno = 0;
         if(file_exists($this->file_path) && is_file($this->file_path)){
             $handle = fopen($this->file_path,'r');
@@ -96,12 +107,39 @@ class NewsletterLogManager implements Nlme{
                     }//while(($file_line = fgets($handle) !== false)){
                     fclose($handle);
                 }//if($this->filesize > 0){
+                return true;
             }//if($handle !== false){
-            else $this->errno = Nlme::ERR_READ_FILE;
+            $this->errno = Nlme::ERR_READ_FILE;
+            return false;
         }//if(file_exists($this->file_path) && is_file($this->file_path)){
-        else $this->errno = Nlme::ERR_INVALID_FILE;    
-        
-        
+        $this->errno = Nlme::ERR_INVALID_FILE;
+        return false;
+    }
+
+    /**
+     * Write the log information on a file
+     * @param array $loginfo
+     * @return bool
+     */
+    public function writeFile(array $loginfo): bool{
+        $this->errno = 0;
+        $handle = fopen($this->file_path,'a');
+        if($handle !== false){
+            $content = array_reduce($loginfo,function($carry,$item){
+                if($item instanceof NewsletterLogInfo){
+                    $carry .= sprintf("SUBJECT: %s RECIPIENT: %s DATE: %s\r\n",$item->getSubject(),$item->getRecipient(),$item->getDate());
+                    return $carry;
+                }
+                return $carry;
+            },'');
+            $writed = fwrite($handle,$content);
+            fclose($handle);
+            if($writed !== false) return true;
+            $this->errno = Nlme::ERR_WRITE_FILE;
+            return false;
+        }//if($handle !== false){
+        $this->errno = Nlme::ERR_WRITE_FILE;
+        return false;
     }
 }
 ?>
