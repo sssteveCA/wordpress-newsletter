035cdb4fc93c5386f1e07ce247fc84f713a46d03
stepp <stepp@mail.com>
2023-07-23 20:43:08 +0200

    Added setLangCheckboxes method to classes getsettings.php

--- classes/settings/getsettings.php
+++ classes/settings/getsettings.php
@@ -23,34 +23,13 @@ class GetSettings implements Gse{
     use ErrorTrait;
 
     private string $html = '';
+    private array $data = [];
 
     public function __construct(){
-        $data = $this->getData();
-    }
+        $this->getData();
+        if($this->errno == 0){
 
-    private function getData(): array{
-        $settings = new Settings(['tableName' => C::TABLE_SETTINGS]);
-        $settings->getSettings();
-        if($settings->getErrno() == 0){
-            return [
-                C::KEY_DONE => true,
-                C::KEY_DATA => [
-                    'lang_status' => $settings->getLangStatus(),
-                    'included_pages_status' => $settings->getIncludedPagesStatus(),
-                    'socials_status' => $settings->getSocialsStatus(),
-                    'social_pages' => $settings->getSocialPages(),
-                    'contact_pages' => $settings->getContactPages(),
-                    'cookie_policy_pages' => $settings->getCookiePolicyPages(),
-                    'privacy_policy_pages' => $settings->getPrivacyPolicyPages(),
-                    'terms_pages' => $settings->getTermsPages(),
-                ]
-            ];
         }
-        else $this->errno = Gse::ERR_GET_DATA;
-        return [
-            C::KEY_DONE => false,
-            C::KEY_MESSAGE => M::ERR_UNAUTHORIZED
-        ];
     }
 
     public function getHtml(){ return $this->html; }
@@ -65,6 +44,68 @@ class GetSettings implements Gse{
                 break;
         }
         return $this->error;
+    }
+
+    private function getData(): void{
+        $settings = new Settings(['tableName' => C::TABLE_SETTINGS]);
+        $settings->getSettings();
+        if($settings->getErrno() == 0){
+            $this->data =  [
+                C::KEY_DONE => true,
+                C::KEY_DATA => [
+                    'lang_status' => $settings->getLangStatus(),
+                    'included_pages_status' => $settings->getIncludedPagesStatus(),
+                    'socials_status' => $settings->getSocialsStatus(),
+                    'social_pages' => $settings->getSocialPages(),
+                    'contact_pages' => $settings->getContactPages(),
+                    'cookie_policy_pages' => $settings->getCookiePolicyPages(),
+                    'privacy_policy_pages' => $settings->getPrivacyPolicyPages(),
+                    'terms_pages' => $settings->getTermsPages(),
+                ]
+            ];
+        }
+        else $this->errno = Gse::ERR_GET_DATA;
+        $this->data = [
+            C::KEY_DONE => false,
+            C::KEY_MESSAGE => M::ERR_UNAUTHORIZED
+        ];
+    }
+
+    private function setLangCheckboxes(): string{
+        $langStatus = $this->data[C::KEY_DATA]['lang_status'];
+        $html =<<<HTML
+<div id="nl_container_langs" class="container">
+    <div class="row">
+        <h5>Lingue</h5>
+    </div>
+    <div class="row flex-column flex-md-row">
+HTML;
+        $checked = ($langStatus['it']) ? ' checked': '';
+        $html .= <<<HTML
+        <div class="col">
+            <input type="checkbox" class="form-check-input" id="nl_cb_lang_it"{$checked}>
+            <label class="form-check-label" for="nl_cb_lang_it">Italiano</label>
+        </div>
+HTML;
+        $checked = ($langStatus['es']) ? ' checked': '';
+        $html .= <<<HTML
+        <div class="col">
+            <input type="checkbox" class="form-check-input" id="nl_cb_lang_es"{$checked}>
+            <label class="form-check-label" for="nl_cb_lang_es">Spagnolo</label>
+        </div>
+HTML;
+        $checked = ($langStatus['en']) ? ' checked': '';
+        $html .= <<<HTML
+        <div class="col">
+            <input type="checkbox" class="form-check-input" id="nl_cb_lang_en"{$checked}>
+            <label class="form-check-label" for="nl_cb_lang_en">Inglese</label>
+        </div>
+HTML;
+        $html .= <<<HTML
+    </div>
+</div>
+HTML;
+        return $html;
     }
 }
 
