96cfce5b562148c5e7e1bd5914ecad9d14b0f047
Stefano <stefano@mail.com>
2023-12-26 15:02:11 +0000

    Removed scripts tag generation from genericHtml

--- classes/htmlcode.php
+++ classes/htmlcode.php
@@ -451,49 +451,17 @@ HTML;
      * Create an HTML page
      * @param string $title page title
      * @param string $body the content inside the body tag
-     * @param string $styleTag (optional) <style> tag content
-     * @param array $styles (optional) an array that contains css files
-     * @param array $sripts (optional) an array that contains js files
      * @return string the HTML page content
      */
-    public static function genericHtml(string $title, string $body, string $styleTag = "",array $styles = [], array $scripts = []):string{
-        $stylesS = "";
-        $scriptsS = "";
-        $content = "";
-        $styles_map = array_map(function($style){
-            return '<link rel="stylesheet" href="'.$style['href'].'">';
-        },$styles);
-        $scripts_map = array_map(function($script){
-            if(isset($script['type'])) $type = 'type="'.$script['type'].'"';
-            else $type = "";
-            return '<script '.$type.' src="'.$script['src'].'"></script>';
-        },$scripts);
-        if(!empty($styles_map)){
-            foreach($styles_map as $style) $stylesS .= $style;
-        }
-        if(!empty($scripts_map)){
-            foreach($scripts_map as $script) $scriptsS .= $script;
-        }
-        $content .= <<<HTML
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>{$title}</title>
-        <meta charset="utf-8">
-        {$stylesS}
-        {$scriptsS}
-        <style>
-            {$styleTag}
-        </style>
-    </head>
-    <body>
-        <div id="primary" class="content-area">
-            <main id="main" class="site-main">
-                {$body}
-            </main>
-        </div>
-    </body>
-</html>
+    public static function genericHtml(string $title, string $body):string{
+        $content = <<<HTML
+<body>
+    <div id="primary" class="content-area">
+        <main id="main" class="site-main">
+            {$body}
+        </main>
+    </div>
+</body>
 HTML;
     return $content;
     }
